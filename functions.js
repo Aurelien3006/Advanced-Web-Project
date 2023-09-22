6b777728e9e8c504a9036715506996211085c7f3
Aurelien <aurelien.broncard@hotmail.fr>
2023-09-22 11:25:46 -0700

    I rewrite index.js, create the function.js with ExportedFunction in it, change package.json so the command 'npm run start' works and add all the necessary files of html and CSS

---
+++ functions.js
@@ -0,0 +1,8 @@
+// functions.js
+function ExportedFunction(id) {
+    // Logic here based on the provided id
+    return `This is the data for ID ${id}`;
+  }
+  
+  module.exports = ExportedFunction;
+ 
--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-console.log("Hello, this is index.js running!");
+const express = require('express');
+const path = require('path');
+
+// Import the function someExportedFunction from functions.js
+const ExportedFunction = require('./functions');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Serve static files inside the 'public' directory
+app.use('/', express.static(path.join(__dirname, 'public')));
+
+// Dynamic endpoint
+app.get('/api/:id', (req, res) => {
+  const id = req.params.id;
+  const response = {
+    // message is the text printed on the page after a click on either button 1 or 2
+    message: `ID ${id}`,
+    data: ExportedFunction(id)
+  };
+  res.json(response);
+});
+
+// Start the server on port 3000
+app.listen(PORT, () => {
+  console.log(`Server started successfully on http://localhost:${PORT}`);
+});
