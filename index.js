a93d07ed6c021676884e9763f321315b84d1e520
Aurelien <aurelien.broncard@hotmail.fr>
2023-10-08 21:48:31 -0700

    Assignement 4

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const path = require('path');
 const ExportedFunction = require('./functions');
 
 const app = express();
+const knex = require('knex');
 const PORT = process.env.PORT || 3000;
 
 // Serve static files inside the 'public' directory
@@ -19,6 +20,64 @@ app.get('/api/:id', (req, res) => {
     data: ExportedFunction(id)
   };
   res.json(response);
+});
+
+
+
+app.post('/api/create', async (req, res) => {
+  const { param1, param2, param3, param4 } = req.body;
+
+  knex('users')
+    .insert({ id: param1, username: param2, email: param3, password: param4})
+    .returning('*') // This is to return the newly-inserted record
+    .then(data => {
+      res.json(data); // Send the newly-created record as a JSON response
+    })
+    .catch(error => {
+      res.status(500).json({ error: error.message });
+    });
+});
+
+app.get('/api/retrieve/:param', async (req, res) => {
+  const { param } = req.params; // Extract parameter from URL
+
+  knex('users')
+    .where('username', 'like', `%${param}%`)
+    .then(data => {
+      res.json(data); // Send the retrieved records as a JSON response
+    })
+    .catch(error => {
+      res.status(500).json({ error: error.message });
+    });
+});
+
+app.put('/api/update/:id', async (req, res) => {
+  const { id } = req.params; // Extract ID from URL
+  const { param1, param2, param3, param4 } = req.body; // Extract parameters from request body
+
+  knex('users')
+    .where({ id })
+    .update({ id: param1, username: param2, email: param3, password: param4})
+    .then(() => {
+      res.json({ success: true }); // Send a success response as JSON
+    })
+    .catch(error => {
+      res.status(500).json({ error: error.message });
+    });
+});
+
+app.delete('/api/delete/:id', async (req, res) => {
+  const { id } = req.params; // Extract ID from URL
+
+  knex('users')
+    .where({ id })
+    .del()
+    .then(() => {
+      res.json({ success: true }); // Send a success response as JSON
+    })
+    .catch(error => {
+      res.status(500).json({ error: error.message });
+    });
 });
 
 // Start the server on port 3000
---
+++ knexfile.js
@@ -0,0 +1,22 @@
+/**
+ * @type { Object.<string, import("knex").Knex.Config> }
+ */
+module.exports = {
+
+  development: {
+    client: 'pg',
+    connection: {
+      database: 'mydatabase',
+      user:     'aurelienbroncard',
+      password: 'Aa00346700@'
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  }
+
+};
